64870a85ae17afd6f687ab4dea25d8bd080c6023
Yash Tyagi <yashtyagi8006@gmail.com>
2025-11-13 21:57:00 +0530

    Recoil Selector Code

--- week-11/class-2/recoil-state-management/src/App.jsx
+++ week-11/class-2/recoil-state-management/src/App.jsx
@@ -1,13 +1,37 @@
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 import "./App.css";
-import { counterAtom } from "./store/atoms/counter";
+import { counterAtom, evenSelector } from "./store/atoms/counter";
 
 function App() {
   return (
     <RecoilRoot>
-      <Counter />
+      <Buttons />
+      <Count />
+      <IsEven />
     </RecoilRoot>
   );
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(counterAtom);
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 2)}>Increase</button>
+      <button onClick={() => setCount((c) => c - 1)}>Decrease</button>
+    </div>
+  );
+}
+
+function Count() {
+  const count = useRecoilValue(counterAtom);
+
+  return <div>{count}</div>;
+}
+
+function IsEven() {
+  const isEven = useRecoilValue(evenSelector);
+
+  return <div>{isEven ? "Even" : "Odd"}</div>;
 }
 
 function Counter() {
--- week-11/class-2/recoil-state-management/src/store/atoms/counter.js
+++ week-11/class-2/recoil-state-management/src/store/atoms/counter.js
@@ -1,6 +1,14 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const counterAtom = atom({
   key: "counterKey",
   default: 0,
+});
+
+export const evenSelector = selector({
+  key: "evenSelectorKey",
+  get: ({ get }) => {
+    const currentCount = get(counterAtom);
+    return currentCount % 2 == 0;
+  },
 });
